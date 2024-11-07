d5af14cb2936a173b01051a0dd725e94528655ab
D-Coder-42 <137105924+D-Coder-42@users.noreply.github.com>
2024-11-07 17:45:00 +0530

    Create PressureTransducer_INT.ino
    Baremetal code to read pressure transducer using interrupts

---
+++ testing/MLH Pressure Transducer/PressureTransducer_INT.ino
@@ -0,0 +1,33 @@
+int adcVal;
+volatile uint16_t overflowCount = 0; // Counter for timer overflows
+
+// triggered whenever ADC conversion is complete
+ISR(ADC_vect)
+{
+  adcVal = ADC;
+  Serial.println(adcVal));
+}
+
+void initADC() {
+  // setting reference voltage and enabling ADC3
+  ADMUX &= ~(1<<REFS1) & ~(1<<MUX3) & ~(1<<MUX2);
+  ADMUX |= (1<<REFS0) | (1<<MUX1) | (1<<MUX0);
+
+  // setting up ADC and prescaler
+  ADCSRA &= ~(1<<ADPS2) & ~(1<<ADPS0);
+  ADCSRA |= (1<<ADEN) | (1<<ADIE) | (1<<ADPS2) | (1<<ADPS0);
+}
+
+int main()
+{
+  Serial.begin(9600);
+  // setting pin A3 as input, pull up disable
+  DDRC &= ~(1<<DDC3);
+  PORTC &= ~(1<<PORTC3);
+  MCUCR |= (1<<PUD);
+
+  initADC();
+  sei();
+
+  while(1);
+}
